164da9404bfbe1b0b07552e18ad00cf5e3e47bed
Luyando Shilukukwa <48803829+Luyandosnt@users.noreply.github.com>
2026-07-31 12:55:00 +0200

    Add offline service worker

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE='geco-color-lane-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
